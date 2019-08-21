483976f609faee65d703561cc6f346d9f856ab8a
Nulide <Nulide@protonmail.com>
2019-08-21 16:48:39 +0200

    Initial commit

---
+++ app/src/main/java/de/nulide/shiftcal/MainActivity.java
@@ -0,0 +1,15 @@
+package de.nulide.shiftcal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
